7d2c8d3eced0d9ce351c838bf892e0facb3a306b
“Dzifa1” <chrisdzifa@gmail.com>
2022-10-20 14:00:20 -0700

    task 3 fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
 * int _islower(int c) -> checks if the character is lowercase
 * @c: a character argument
+* Description: checka if character is lowr case
 * Return: return 1 if lower case, 0 if otherwise
 */
 
