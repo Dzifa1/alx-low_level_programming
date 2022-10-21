569977e4cc35cdf636dac08935b12b80022370ec
“Dzifa1” <chrisdzifa@gmail.com>
2022-10-20 18:25:58 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+* _abs - computes the absolute value of integer
+* @c : character to be computed
+* Return: Absolute Value
+*/
+
+int _abs(int c)
+{
+		if (c >= 0)
+		return (c);
+	else
+		return (c * -1);
+}
+
