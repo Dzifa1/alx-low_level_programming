643cd159e66d188de854a728f915787ef80992a5
“Dzifa1” <chrisdzifa@gmail.com>
2022-10-20 19:10:51 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+* print_last_digit - computes the value of the last digit
+* @c : character to be print
+* Return: Value of last digit
+*/
+
+int print_last_digit(int c)
+{
+	int n;
+	if (c < 0)
+		n = -1 * (c % 10);
+	else
+		n = c % 10;
+
+	_putchar((n % 10) + '0');
+
+	return (n % 10);
+}
