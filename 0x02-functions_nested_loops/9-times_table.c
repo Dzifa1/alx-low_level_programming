2b8114c00d22b273a2778f28aa06eccf7ca94116
“Dzifa1” <chrisdzifa@gmail.com>
2022-10-21 07:10:36 -0700

    task 9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,11 @@ void times_table(void)
 {
 	int i, j, n;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
-
 			if ((n / 10) == 0)
 			{
 				if (j == 0)
@@ -34,7 +33,7 @@ void times_table(void)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-				if (j < 9)
+					if (j < 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
