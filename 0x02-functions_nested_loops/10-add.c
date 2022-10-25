8761674c9649530e3f9fe849c347f55cc3be85b0
“Dzifa1” <chrisdzifa@gmail.com>
2022-10-25 04:22:13 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @c : integer a
+ * @j : integer b
+ * Description: adds two integers and prints results
+ * Return: Add c and j
+ */
+
+int add(int c, int j)
+{
+
+	return (c + j);
+}
