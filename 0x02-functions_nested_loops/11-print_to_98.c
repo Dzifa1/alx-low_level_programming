5593a0638d1b488ea1a93cb5e7ef2bbd3f8fd669
“Dzifa1” <chrisdzifa@gmail.com>
2022-10-25 06:30:27 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_to_98 - print natural numbers
+ * @c : number to be printed
+ * function that prints all natural numbers from c to 98
+ * Return: Always 0
+ */
+
+void print_to_98(int c)
+{
+	if (c <= 98)
+	{
+		for (;c <= 98; c++)
+		{
+			printf("%d\n", c);
+
+			if (c == 98)
+				continue;
+			printf(",");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (;c >= 98; c--)
+		{
+			printf("%d\n", c--);
+
+			if (c == 98)
+				continue;
+			printf(",");
+		}
+		printf("\n");
+	}
+}
